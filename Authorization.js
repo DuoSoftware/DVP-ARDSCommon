eb83bba9714b07b8affe7a56f7555ae2138bc1e3
heshanindika <k.heshan.indika@gmail.com>
2016-03-03 11:52:22 +0530

    change Authorization.js

--- Authorization.js
+++ Authorization.js
@@ -1,20 +1,30 @@
 ﻿var infoLogger = require('./InformationLogger.js');
 
-var ValidateAuthToken = function (authHeader, callback) {
+var ValidateAuthToken = function (req, callback) {
     var tenantId = 1;
     var companyId = 1;
     try {
-        var authInfo = authHeader.split("#");
-        
-        if (authInfo.length >= 2) {
-            tenantId = authInfo[0];
-            companyId = authInfo[1];
+        if(req.header('companyinfo')){
+            var internalAuthHeader = req.header('companyinfo');
+            var authInfo = internalAuthHeader.split(":");
+
+            if (authInfo.length >= 2) {
+                tenantId = authInfo[0];
+                companyId = authInfo[1];
+            }
+            callback(null, companyId, tenantId);
+        }else if(req.user.Company && req.user.Tenant){
+            tenantId = req.user.Company;
+            companyId = req.user.Tenant;
+            callback(null, companyId, tenantId);
+        }else {
+            callback(new Error("Invalid company or tenant"), companyId, tenantId);
         }
     }
     catch (ex) {
         infoLogger.ReqResLogger.log('error', 'Exception occurred -  Data - %s ', "authorization", ex);
+        callback(ex, companyId, tenantId);
     }
-    callback(companyId, tenantId);
 };
 
 
