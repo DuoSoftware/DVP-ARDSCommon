8b6ed44784bcebc2ace828874a1dac80127980fa
heshanindika <k.heshan.indika@gmail.com>
2017-07-18 15:01:35 +0530

    save resource break duration

--- ResourceStateMapper.js
+++ ResourceStateMapper.js
@@ -3,6 +3,7 @@ var util = require('util');
 var infoLogger = require('./InformationLogger.js');
 var resourceService = require('./services/resourceService');
 var scheduleWorkerHandler = require('./ScheduleWorkerHandler');
+var moment = require('moment');
 
 var SetResourceState = function (logKey, company, tenant, resourceId, resourceName, state, reason, callback) {
     infoLogger.DetailLogger.log('info', '%s ************************* Start SetResourceState *************************', logKey);
@@ -14,8 +15,7 @@ var SetResourceState = function (logKey, company, tenant, resourceId, resourceNa
             console.log(err);
         }
         else {
-            var date = new Date();
-            resultObj.StateChangeTime = date.toISOString();
+
             var strObj = JSON.stringify(resultObj);
             redisHandler.SetObj(logKey, StateKey, strObj, function (err, result) {
                 if (err != null) {
@@ -44,8 +44,8 @@ var SetResourceState = function (logKey, company, tenant, resourceId, resourceNa
 };
 
 var processState = function (logKey, stateKey, internalAccessToken, resourceId, resourceName, state, reason, callback) {
-    var statusObj = {ResourceName: resourceName, State: state, Reason: reason};
-
+    var date = new Date();
+    var statusObj = {ResourceName: resourceName, State: state, Reason: reason, StateChangeTime: date.toISOString()};
     redisHandler.GetObj(logKey, stateKey, function (err, statusStrObj) {
         if(err){
             statusObj.Mode = 'Offline';
@@ -54,11 +54,31 @@ var processState = function (logKey, stateKey, internalAccessToken, resourceId, 
             if(statusStrObj) {
                 var statusObjR = JSON.parse(statusStrObj);
                 statusObj.Mode = statusObjR.Mode;
+
+                if (statusObjR && statusObjR.State === "NotAvailable" && statusObjR.Reason.toLowerCase().indexOf('break') > -1) {
+                    var duration = moment(statusObj.StateChangeTime).diff(moment(statusObjR.StateChangeTime), 'seconds');
+                    resourceService.AddResourceStatusDurationInfo(internalAccessToken, resourceId, "ResourceStatus", statusObjR.State, statusObjR.Reason, '', '', duration, function () {
+                        if (err) {
+                            console.log("AddResourceStatusDurationInfo Failed.", err);
+                        } else {
+                            console.log("AddResourceStatusDurationInfo Success.");
+                        }
+                    });
+                }
+
                 if(state === "NotAvailable" && reason === "UnRegister") {
                     statusObj.Mode = "Offline";
-                    if (statusObjR && statusObjR.State === "NotAvailable") {
+                    if (statusObjR && statusObjR.State === "NotAvailable" && statusObjR.Reason.toLowerCase().indexOf('break') > -1) {
                         resourceService.AddResourceStatusChangeInfo(internalAccessToken, resourceId, "ResourceStatus", "Available", "endBreak", {SessionId:"", Direction:""}, function (err, result, obj) {
                             callback(null, statusObj);
+                        });
+                        var duration1 = moment(statusObj.StateChangeTime).diff(moment(statusObjR.StateChangeTime), 'seconds');
+                        resourceService.AddResourceStatusDurationInfo(internalAccessToken, resourceId, "ResourceStatus", statusObjR.State, statusObjR.Reason, '', '', duration1, function () {
+                            if (err) {
+                                console.log("AddResourceStatusDurationInfo Failed.", err);
+                            } else {
+                                console.log("AddResourceStatusDurationInfo Success.");
+                            }
                         });
                     } else {
                         callback(null, statusObj);
