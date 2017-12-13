fc34f5cb7b15bf3a4b07c1f7fcfa7e8460bfc444
heshanindika <k.heshan.indika@gmail.com>
2017-12-13 12:22:40 +0530

    fix log value

--- RedisHandler.js
+++ RedisHandler.js
@@ -1182,7 +1182,7 @@ var CheckHashFieldExists = function (logKey, hashkey, field, callback) {
 
 var GetHashValue = function(logKey, hashKey, field, callback) {
     infoLogger.DetailLogger.log('info', '%s --------------------------------------------------', logKey);
-    infoLogger.DetailLogger.log('info', '%S GetHashValue - hashKey: %s :: field: %s', logKey, hashKey, field);
+    infoLogger.DetailLogger.log('info', '%s GetHashValue - hashKey: %s :: field: %s', logKey, hashKey, field);
 
     client.hget(hashKey, field, function (err, result) {
         if (err) {
